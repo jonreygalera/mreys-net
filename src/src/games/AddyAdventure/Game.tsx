49eec152ad93436f46509078d784bf936a6cf65e
Jon Rey Galera <jonreygalera@gmail.com>
2025-03-13 09:02:19 +0800

    remove debug

--- src/src/games/AddyAdventure/Game.tsx
+++ src/src/games/AddyAdventure/Game.tsx
@@ -12,7 +12,7 @@ export const basedScreenSize = {
 export const gameConfig: IGameConfig = {
   width: innerWidth,
   height: innerHeight,
-  debugger: { debug: true, indexOnly: true}
+  debugger: { debug: false, indexOnly: true}
 };
 
 const gravity = 500;
